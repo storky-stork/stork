fefcbb4759fb49c92158b2f12cab2faa7780edc9
Isragab <yvbenitah@gmail.com>
2022-04-26 23:42:16 +0300

    CHANGE TO BASH

--- packages/nest-server/src/app/app.service.ts
+++ packages/nest-server/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Ready to work2!' };
+    return { message: '-Ready to work2!' };
   }
 }
